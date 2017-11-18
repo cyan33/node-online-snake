dce4e6f42465fa526f7d636faac832ad4bcc3208
thomasyimgit <thomasyim94@gmail.com>
2017-11-18 16:01:42 -0500

    refactor code structure

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,8 @@ const express = require('express');
 const app = express();
 
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
-const { update, moveSnake, initSnake } = require('./serverSnakeHelper')
+const { createIO } = require('./network')
 
 // static files
 app.use('/dist', express.static('dist'))
@@ -15,40 +14,8 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../../index.html'));
 });
 
-const state = {
-  // scene: {
-  //   food: { x, y },
-  //   spoiledFood: { }
-  // },
-  // player1: { segments: [], direction: 'RIGHT' },
-  // player2: {},
-}
-
-let hasPlayer = false
-
-io.on('connection', (socket) => {
-  console.log(`a user connected, id: ${socket.id}`);
-  hasPlayer = true
-
-  state[socket.id] = {
-    direction: 'RIGHT',
-    segments: initSnake(),
-  }
-
-  socket.on('change_direction', (direction) => {
-    state[socket.id].direction = direction
-  })
-
-  socket.on('disconnect', () => {
-    delete state[socket.id]
-    console.log('user disconnected');
-  });
-});
-
-setInterval(() => {
-  update(state)
-  io.sockets.emit('render', state)
-}, 250)
+// io stuff
+createIO(http).listen()
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
---
+++ src/server/state.js
@@ -0,0 +1,9 @@
+// model for game state
+module.exports = {
+  // scene: {
+  //   food: { x, y },
+  //   spoiledFood: { }
+  // },
+  // player1: { segments: [], direction: 'RIGHT' },
+  // player2: {},
+}
